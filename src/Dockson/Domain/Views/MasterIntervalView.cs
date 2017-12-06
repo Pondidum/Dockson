ea5ca7bc64faf24dde634dc5fb7454471116a920
andy <andy@andydote.co.uk>
2017-12-06 14:39:39 +0200

    groupSummary: move to separate file

--- src/Dockson/Domain/Views/MasterIntervalView.cs
+++ src/Dockson/Domain/Views/MasterIntervalView.cs
@@ -6,14 +6,4 @@ namespace Dockson.Domain.Views
 	public class MasterIntervalView : Dictionary<string, GroupSummary>
 	{
 	}
-
-	public class GroupSummary
-	{
-		public Dictionary<DayDate, Summary> Daily { get; set; }
-
-		public GroupSummary()
-		{
-			Daily = new Dictionary<DayDate, Summary>();
-		}
-	}
 }
