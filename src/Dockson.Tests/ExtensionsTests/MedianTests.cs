63cca3aba9148eb050cfada42c7821daa5a9f567
andy <andy@andydote.co.uk>
2017-12-08 20:29:19 +0200

    extensions: replace median() with less performant, but working version

---
+++ src/Dockson.Tests/ExtensionsTests/MedianTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Shouldly;
+using Xunit;
+
+namespace Dockson.Tests.ExtensionsTests
+{
+	public class MedianTests
+	{
+		public static IEnumerable<object[]> TestCases => new[]
+		{
+			new object[] { 176m, new double[] { 176 } },
+			new object[] { 5.5m, new double[] { 5, 6 } },
+			new object[] { 3m, new double[] { 2, 3, 4 } },
+			new object[] { 2.5m, new double[] { 1, 2, 3, 4 } },
+			new object[] { 3m, new double[] { 1, 2, 3, 4, 5 } },
+			new object[] { 15m, new double[] { 10, 20 } },
+		};
+
+		[Theory]
+		[MemberData(nameof(TestCases))]
+		public void When_checking_the_median(double expected, double[] values)
+		{
+			values.Median().ShouldBe(expected, tolerance: 0.005);
+		}
+	}
+}
--- src/Dockson/Extensions.cs
+++ src/Dockson/Extensions.cs
@@ -16,7 +16,7 @@ namespace Dockson
 
 		public static DateTime PreviousMonday(this DateTime self)
 		{
-			var daysToRemove = ((int) self.DayOfWeek  - (int) DayOfWeek.Monday+ 7) % 7;
+			var daysToRemove = ((int)self.DayOfWeek - (int)DayOfWeek.Monday + 7) % 7;
 
 			return self.AddDays(-daysToRemove);
 		}
@@ -44,73 +44,17 @@ namespace Dockson
 				: deviation;
 		}
 
-		// likewise
-		// https://stackoverflow.com/a/22702269/1500
-
-		/// <summary>
-		/// Partitions the given list around a pivot element such that all elements on left of pivot are <= pivot
-		/// and the ones at thr right are > pivot. This method can be used for sorting, N-order statistics such as
-		/// as median finding algorithms.
-		/// Pivot is selected ranodmly if random number generator is supplied else its selected as last element in the list.
-		/// Reference: Introduction to Algorithms 3rd Edition, Corman et al, pp 171
-		/// </summary>
-		private static int Partition<T>(this IList<T> list, int start, int end, Random rnd = null) where T : IComparable<T>
-		{
-			if (rnd != null)
-				list.Swap(end, rnd.Next(start, end + 1));
-
-			var pivot = list[end];
-			var lastLow = start - 1;
-			for (var i = start; i < end; i++)
-			{
-				if (list[i].CompareTo(pivot) <= 0)
-					list.Swap(i, ++lastLow);
-			}
-
-			list.Swap(end, ++lastLow);
-			return lastLow;
-		}
-
-		/// <summary>
-		/// Returns Nth smallest element from the list. Here n starts from 0 so that n=0 returns minimum, n=1 returns 2nd smallest element etc.
-		/// Note: specified list would be mutated in the process.
-		/// Reference: Introduction to Algorithms 3rd Edition, Corman et al, pp 216
-		/// </summary>
-		private static T NthOrderStatistic<T>(this IList<T> list, int n, Random rnd = null) where T : IComparable<T>
-		{
-			return NthOrderStatistic(list, n, 0, list.Count - 1, rnd);
-		}
-
-		private static T NthOrderStatistic<T>(this IList<T> list, int n, int start, int end, Random rnd) where T : IComparable<T>
-		{
-			while (true)
-			{
-				var pivotIndex = list.Partition(start, end, rnd);
-				if (pivotIndex == n)
-					return list[pivotIndex];
-
-				if (n < pivotIndex)
-					end = pivotIndex - 1;
-				else
-					start = pivotIndex + 1;
-			}
-		}
-
-		private static void Swap<T>(this IList<T> list, int i, int j)
-		{
-			if (i == j) //This check is not required but Partition function may make many calls so its for perf reason
-				return;
-			var temp = list[i];
-			list[i] = list[j];
-			list[j] = temp;
-		}
-
+		// not the most performant version, but honestly, I don't care at this point
 		public static double Median(this IEnumerable<double> sequence)
 		{
 			var list = sequence.ToList();
-			var mid = (list.Count - 1) / 2;
+			list.Sort();
 
-			return list.NthOrderStatistic(mid);
+			var mid = list.Count / 2;
+
+			return list.Count % 2 != 0
+				? list[mid]
+				: (list[mid] + list[mid - 1]) / 2;
 		}
 	}
 }
