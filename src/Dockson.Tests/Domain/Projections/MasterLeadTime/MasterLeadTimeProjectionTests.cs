248beb45d44d6110966712723725d91360b7359f
andy <andy@andydote.co.uk>
2017-12-13 21:00:10 +0200

    masterLeadTimeProjectionTests: use eventSource

--- src/Dockson.Tests/Domain/Projections/MasterLeadTime/MasterLeadTimeProjectionTests.cs
+++ src/Dockson.Tests/Domain/Projections/MasterLeadTime/MasterLeadTimeProjectionTests.cs
@@ -9,56 +9,42 @@ namespace Dockson.Tests.Domain.Projections.MasterLeadTime
 {
 	public class MasterLeadTimeProjectionTests
 	{
-		private const string Group = "omg-service";
-
 		private readonly MasterLeadTimeView _view;
-		private readonly MasterLeadTimeProjection _projection;
-		private readonly DateTime _now;
+		private readonly EventSource _service;
 
 		public MasterLeadTimeProjectionTests()
 		{
 			_view = new MasterLeadTimeView();
-			_projection = new MasterLeadTimeProjection(_view);
-			_now = new DateTime(2017, 11, 30, 11, 47, 00);
+			var projection = new MasterLeadTimeProjection(_view);
+			_service = new EventSource(projection);
 		}
 
 		[Fact]
 		public void When_projecting_one_commit()
 		{
-			_projection.Project(CreateCommit(TimeSpan.Zero, TimeSpan.FromHours(1)));
-			var day = new DayDate(_now);
+			_service.MasterCommit(sinceFeatureCommit: 1.Hour());
+			var day = new DayDate(_service.Timestamp);
 
 			_view.ShouldSatisfyAllConditions(
-				() => _view[Group].Daily[day].Median.ShouldBe(TimeSpan.FromHours(1).TotalMinutes),
-				() => _view[Group].Daily[day].Deviation.ShouldBe(0)
+				() => _view[_service.Name].Daily[day].Median.ShouldBe(1.Hour().TotalMinutes),
+				() => _view[_service.Name].Daily[day].Deviation.ShouldBe(0)
 			);
 		}
 
 		[Fact]
 		public void When_projecting_two_commits()
 		{
-			_projection.Project(CreateCommit(TimeSpan.Zero, TimeSpan.FromHours(1)));
-			_projection.Project(CreateCommit(TimeSpan.FromHours(4), TimeSpan.FromHours(5)));
+			var day = new DayDate(_service.Timestamp);
+
+			_service
+				.MasterCommit(sinceFeatureCommit: 1.Hour())
+				.Advance(4.Hours())
+				.MasterCommit(sinceFeatureCommit: 5.Hours());
+
+			_view.ShouldSatisfyAllConditions(
+				() => _view[_service.Name].Daily[day].Median.ShouldBe(3.Hours().TotalMinutes),
+				() => _view[_service.Name].Daily[day].Deviation.ShouldBe(169.70, tolerance: 0.01)
+			);
 		}
-
-		private MasterCommit CreateCommit(TimeSpan featureTime, TimeSpan masterTime) => new MasterCommit(
-			CreateNotification(masterTime, "master"),
-			CreateNotification(featureTime, "feature-whatever")
-		);
-
-		private Notification CreateNotification(TimeSpan offset, string branch) => new Notification
-		{
-			Type = Stages.Commit,
-			Timestamp = _now.Add(offset),
-			Source = "github",
-			Name = "SomeService",
-			Version = "1.0.0",
-			Groups = { Group },
-			Tags =
-			{
-				{ "commit", Guid.NewGuid().ToString() },
-				{ "branch", branch }
-			}
-		};
 	}
 }
