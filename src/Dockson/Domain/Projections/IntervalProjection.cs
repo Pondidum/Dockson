2d970afee1fd9e54f4196e2aea83ee7b6b78e172
andy <andy@andydote.co.uk>
2017-12-30 14:04:34 +0200

    projections: remove old view types

--- src/Dockson/Domain/Projections/IntervalProjection.cs
+++ src/Dockson/Domain/Projections/IntervalProjection.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using Dockson.Domain.Views;
 
 namespace Dockson.Domain.Projections
 {
--- src/Dockson/Domain/Projections/LeadTimeProjection.cs
+++ src/Dockson/Domain/Projections/LeadTimeProjection.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using Dockson.Domain.Views;
 
 namespace Dockson.Domain.Projections
 {
