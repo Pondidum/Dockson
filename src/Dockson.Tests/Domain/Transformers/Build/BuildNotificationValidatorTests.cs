55c73a1b2f75547667fec7976d6dee4eccaddc9f
andy <andy@andydote.co.uk>
2018-01-04 19:44:57 +0200

    buildNotificationValidator: add tests

---
+++ src/Dockson.Tests/Domain/Transformers/Build/BuildNotificationValidatorTests.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using Dockson.Domain.Transformers.Build;
+using Shouldly;
+using Xunit;
+
+namespace Dockson.Tests.Domain.Transformers.Build
+{
+	public class BuildNotificationValidatorTests
+	{
+		private string[] Validate(BuildNotification notification) => new BuildNotificationValidator()
+			.Validate(notification)
+			.ToArray();
+
+		[Fact]
+		public void When_the_status_is_missing()
+		{
+			Validate(new BuildNotification { }).ShouldBe(new[]
+			{
+				"You must specify the status of the build (success or failure)"
+			});
+		}
+
+		[Fact]
+		public void When_the_status_is_present()
+		{
+			Validate(new BuildNotification { Status = "success" }).ShouldBeEmpty();
+		}
+	}
+}
