c985966d9f47cb283e21f5a287acf10c16c47bea
andy <andy@andydote.co.uk>
2018-01-06 16:56:46 +0200

    ui: clear the dashboard, not sure what to put there yet

--- src/Dockson/ClientApp/Dashboard/index.js
+++ src/Dockson/ClientApp/Dashboard/index.js
@@ -1,30 +1,5 @@
 import React, { Component } from "react";
-import { connect } from "react-redux";
-import { listAllGroups } from "../Groups/actions";
 
-const mapStateToProps = state => {
-  return state.groups;
-};
+const Dashboard = () => <div />;
 
-const mapDispatchToProps = dispatch => {
-  return {
-    listAllGroups: () => dispatch(listAllGroups())
-  };
-};
-
-class Dashboard extends Component {
-  componentDidMount() {
-    this.props.listAllGroups();
-  }
-
-  render() {
-    const groups = this.props.groups || [];
-    return (
-      <ul className="list-unstyled row">
-        {groups.map((group, index) => <h4 key={index}>{group}</h4>)}
-      </ul>
-    );
-  }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(Dashboard);
+export default Dashboard;
