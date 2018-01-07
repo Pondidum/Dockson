67589375fe4256927d506be774adedb6f6f9c094
andy <andy@andydote.co.uk>
2018-01-07 15:10:21 +0200

    preProjector: only set notification.timestamp if not set

---
+++ src/Dockson.Tests/Domain/PreProjectorTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Dockson.Domain;
+using Shouldly;
+using Xunit;
+
+namespace Dockson.Tests.Domain
+{
+	public class PreProjectorTests
+	{
+		private readonly PreProjector _projector;
+		private readonly DateTime _timestamp;
+
+		public PreProjectorTests()
+		{
+			_timestamp = DateTime.UtcNow;
+			_projector = new PreProjector(new InnerProjector(), () => _timestamp);
+		}
+
+		[Fact]
+		public void When_the_notification_timestamp_is_not_set()
+		{
+			var notification = new Notification();
+			_projector.Project(notification);
+
+			notification.Timestamp.ShouldBe(_timestamp);
+		}
+
+		[Fact]
+		public void When_the_notification_timestamp_is_set()
+		{
+			var existing = DateTime.UtcNow.AddDays(-5);
+			var notification = new Notification { Timestamp = existing };
+			_projector.Project(notification);
+
+			notification.Timestamp.ShouldBe(existing);
+		}
+
+		private class InnerProjector : IProjector
+		{
+			public void Project(Notification notification)
+			{
+			}
+		}
+	}
+}
---
+++ src/Dockson/Domain/PreProjector.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Dockson.Domain
+{
+	public class PreProjector : IProjector
+	{
+		private readonly IProjector _internal;
+		private readonly Func<DateTime> _getDate;
+
+		public PreProjector(IProjector @internal, Func<DateTime> getDate = null)
+		{
+			_internal = @internal;
+			_getDate = getDate ?? (() => DateTime.UtcNow);
+		}
+
+		public void Project(Notification notification)
+		{
+			if (notification.Timestamp == DateTime.MinValue)
+				notification.Timestamp = _getDate();
+
+			_internal.Project(notification);
+		}
+	}
+}
