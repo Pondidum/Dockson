3698c6e90e2ea5fac0925740f679ada75ed2f010
andy <andydavies2020@gmail.com>
2018-01-11 22:31:52 +0200

    scripts: basic commit import script done

--- scripts/commits.sh
+++ scripts/commits.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 
-pushd $1
+apiUrl="http://localhost:5000/api/log/commit"
+serviceName="lindorff-generator"
 
-git log --merges --pretty="%H %P" | while read line; do
+pushd $1 > /dev/null
 
-  master=$(echo $line | cut -f1 -d ' ')
-  branch=$(echo $line | cut -f3 -d ' ')
+git log --merges --pretty="%H %P" --reverse | while read line; do
 
-  echo "branch $branch -> master $master"
+  masterSha=$(echo $line | cut -f1 -d ' ')
+  branchSha=$(echo $line | cut -f3 -d ' ')
+
+  masterName="master"
+  branchName=$(git log --format=%B $masterSha -n 1 | head -n1 | grep -oP "(?<=from ).*" | cut -f2 -d '/')
+
+  masterTimestamp=$(git show -s --format="%ci" $masterSha)
+  branchTimestamp=$(git show -s --format="%ci" $branchSha)
+
+  masterJson="{\"timestamp\": \"$masterTimestamp\", \"name\": \"$serviceName\", \"version\": \"1.0.0\", \"branch\":\"$masterName\", \"commit\":\"$branchSha\"}"
+  branchJson="{\"timestamp\": \"$branchTimestamp\", \"name\": \"$serviceName\", \"version\": \"1.0.0\", \"branch\":\"$branchName\", \"commit\":\"$branchSha\"}"
+
+  curl "$apiUrl" -X POST -H "Content-Type: application/json" -d "$branchJson"
+  curl "$apiUrl" -X POST -H "Content-Type: application/json" -d "$masterJson"
 done
 
-popd
+popd > /dev/null
