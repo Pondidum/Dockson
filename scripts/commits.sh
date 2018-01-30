8656ffdf7e001827de65ecb1f573bca4482b9d12
andy <andydavies2020@gmail.com>
2018-01-30 18:22:43 +0200

    scripts: only post a branch if the name is valid

--- scripts/commits.sh
+++ scripts/commits.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 apiUrl="http://localhost:5000/api/log/commit"
-serviceName="lindorff-generator"
+serviceName="contract"
 
 pushd $1 > /dev/null
 
@@ -13,14 +13,19 @@ git log --merges --pretty="%H %P" --reverse | while read line; do
   masterName="master"
   branchName=$(git log --format=%B $masterSha -n 1 | head -n1 | grep -oP "(?<=from ).*" | cut -f2 -d '/')
 
-  masterTimestamp=$(git show -s --format="%ci" $masterSha)
-  branchTimestamp=$(git show -s --format="%ci" $branchSha)
+  if [ "$branchName" != "" ]; then
 
-  masterJson="{\"timestamp\": \"$masterTimestamp\", \"name\": \"$serviceName\", \"version\": \"1.0.0\", \"branch\":\"$masterName\", \"commit\":\"$branchSha\"}"
-  branchJson="{\"timestamp\": \"$branchTimestamp\", \"name\": \"$serviceName\", \"version\": \"1.0.0\", \"branch\":\"$branchName\", \"commit\":\"$branchSha\"}"
+    masterTimestamp=$(git show -s --format="%ci" $masterSha)
+    branchTimestamp=$(git show -s --format="%ci" $branchSha)
 
-  curl "$apiUrl" -X POST -H "Content-Type: application/json" -d "$branchJson"
-  curl "$apiUrl" -X POST -H "Content-Type: application/json" -d "$masterJson"
+    masterJson="{\"timestamp\": \"$masterTimestamp\", \"name\": \"$serviceName\", \"version\": \"1.0.0\", \"branch\":\"$masterName\", \"commit\":\"$branchSha\"}"
+    branchJson="{\"timestamp\": \"$branchTimestamp\", \"name\": \"$serviceName\", \"version\": \"1.0.0\", \"branch\":\"$branchName\", \"commit\":\"$branchSha\"}"
+
+    curl "$apiUrl" -X POST -H "Content-Type: application/json" -d "$branchJson"
+    curl "$apiUrl" -X POST -H "Content-Type: application/json" -d "$masterJson"
+
+  fi
+
 done
 
 popd > /dev/null
