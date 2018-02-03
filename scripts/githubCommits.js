938764610d5d9c402867d7aa85a7e5c10e1c9697
andy <andydavies2020@gmail.com>
2018-02-03 20:36:13 +0200

    scripts: add pagination support to github script

--- scripts/githubCommits.js
+++ scripts/githubCommits.js
@@ -4,19 +4,45 @@ import fetch from "node-fetch";
 const owner = argv.owner;
 const repo = argv.repo;
 
+const parseLinks = linksHeader =>
+  linksHeader.split(",").reduce((all, link) => {
+    const match = link.match(/\<(.*)\>.*rel="(.*)"/);
+    const url = match[1];
+    const rel = match[2];
+
+    all[rel] = url;
+    return all;
+  }, {});
+
+const appendPulls = dto => {
+  console.log(`adding ${dto.length} items`);
+  return Promise.resolve();
+};
+
+const requestPage = url =>
+  fetch(url, {
+    headers: { Authorization: `token ${process.env.DOCKSON_GITHUB}` }
+  }).then(res =>
+    res
+      .json()
+      .then(appendPulls)
+      .then(() => {
+        const links = parseLinks(res.headers.get("Link"));
+
+        if (links.next) {
+          return requestPage(links.next);
+        }
+      })
+  );
+
 const run = () => {
   if (!owner || !repo) {
     console.error("You must specifiy --owner and --repo");
     return;
   }
 
-  return fetch(
-    `https://api.github.com/repos/${owner}/${repo}/pulls?state=all`,
-    {
-      headers: {
-        Authorization: `token: ${process.env.DOCKSON_GITHUB}`
-      }
-    }
+  return requestPage(
+    `https://api.github.com/repos/${owner}/${repo}/pulls?state=all`
   );
 };
 
